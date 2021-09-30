60e53fa53751f40b48baa2e00ae108a4c399b060
matheronn <matheusem94@gmail.com>
2021-09-29 21:14:32 -0300

    Projeto finalizado

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		if (n % 2 == 0) {
+			System.out.println("PAR");
+		}
+		else {
+			System.out.println("IMPAR");
+		}
+
+		sc.close();
+
+	}
+
+}
